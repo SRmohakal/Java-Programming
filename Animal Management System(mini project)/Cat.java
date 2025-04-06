2d1aa9e5321dfee4ad7976953ec70ca9013fea35
Shourov Roy <138048581+SRmohakal@users.noreply.github.com>
2025-04-06 23:12:36 +0600

    Cat.java

---
+++ Animal Management System(mini project)/Cat.java
@@ -0,0 +1,11 @@
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(getName() + " says: Meow!");
+    }
+}
